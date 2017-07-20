2bdb4936c86738155f17cb970eecb5dc0f9e3828
dev <dev@debian>
2017-07-20 09:37:41 +0200

    First commit.

---
+++ readonlyemailaddress.php
@@ -0,0 +1,172 @@
+<?php
+
+require_once 'readonlyemailaddress.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function readonlyemailaddress_civicrm_config(&$config) {
+  _readonlyemailaddress_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function readonlyemailaddress_civicrm_xmlMenu(&$files) {
+  _readonlyemailaddress_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function readonlyemailaddress_civicrm_install() {
+  _readonlyemailaddress_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function readonlyemailaddress_civicrm_postInstall() {
+  _readonlyemailaddress_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function readonlyemailaddress_civicrm_uninstall() {
+  _readonlyemailaddress_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function readonlyemailaddress_civicrm_enable() {
+  _readonlyemailaddress_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function readonlyemailaddress_civicrm_disable() {
+  _readonlyemailaddress_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function readonlyemailaddress_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _readonlyemailaddress_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function readonlyemailaddress_civicrm_managed(&$entities) {
+  _readonlyemailaddress_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function readonlyemailaddress_civicrm_caseTypes(&$caseTypes) {
+  _readonlyemailaddress_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function readonlyemailaddress_civicrm_angularModules(&$angularModules) {
+  _readonlyemailaddress_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function readonlyemailaddress_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _readonlyemailaddress_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function readonlyemailaddress_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function readonlyemailaddress_civicrm_navigationMenu(&$menu) {
+  _readonlyemailaddress_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => ts('The Page', array('domain' => 'org.ilga.readonlyemailaddress')),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _readonlyemailaddress_civix_navigationMenu($menu);
+} // */
+
+/**
+ * Implements hook_civicrm_buildForm().
+ *
+ * Set a default value for an event price set field.
+ *
+ * @param string $formName
+ * @param CRM_Core_Form $form
+ */
+function readonlyemailaddress_civicrm_buildForm($formName, &$form) {    
+    if('CRM_Contribute_Form_Contribution_Main' == $formName){
+        if($form->getAction() == CRM_Core_Action::ADD){
+            
+            foreach($form->_elements as $key => $element){
+                if('Email Address' == $element->_label){
+                  $form->_elements[$key]->_flagFrozen = true;
+                }
+            }
+        }        
+    }
+}
